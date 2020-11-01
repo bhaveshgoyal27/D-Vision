c4a9a7e814d593d559181955303812f449fc347f
BHAVESH GOYAL <bhavstar99@gmail.com>
2020-11-01 10:08:52 +0530

    emotion detection

---
+++ emotion.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from tensorflow.keras import models
+#from keras.models import load_model
+
+classifier = models.load_model('emotion_little_vgg_3.h5')
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+class_labels = {0: 'Angry', 1: 'Fear', 2: 'Happy', 3: 'Neutral', 4: 'Sad', 5: 'Surprise'}
+def face_detector(img):
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces is ():
+        return (0,0,0,0), np.zeros((48,48), np.uint8), img
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+
+    try:
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation = cv2.INTER_AREA)
+    except:
+        return (x,w,y,h), np.zeros((48,48), np.uint8), img
+    return (x,w,y,h), roi_gray, img
+
+def emotion(frame):
+    rect, face, image = face_detector(frame)
+    if np.sum([face]) != 0.0:
+        roi = face.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+
+        # make a prediction on the ROI, then lookup the class
+        preds = classifier.predict(roi)[0]
+        label = class_labels[preds.argmax()]  
+        label_position = (rect[0] + int((rect[1]/2)), rect[2] + 25)
+        cv2.putText(image, label, label_position , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
+    else:
+        cv2.putText(image, "No Face Found", (20, 60) , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
+
+    return image
